b395942fb86bc3069aa9afabdce12f6848cce4ee
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-05-25 10:19:34 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+function winCheck(element){
+    return (element.result === 'W')
+}
+
+function superbowlWin(record){
+    if (record.find(winCheck)){
+        return record.find(winCheck).year;
+    }
+    else{
+        return undefined;
+    }
+}
